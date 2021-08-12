3593dd04d987ee389ffeb48d2858a85893c3b77a
caolaidong <1026190993@qq.com>
2021-08-12 18:14:54 +0800

    no message

---
+++ SwiftLaiDong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLaiDong
+//
+//  Created by 曹来东 on 2021/8/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.random;
+    }
+
+
+}
+
